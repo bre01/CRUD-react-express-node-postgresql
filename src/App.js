647b83037f95be3d9a50dafd106c70e309b87fbb
bre01 <iwanttosignup321@gmail.com>
2024-06-26 18:44:09 +0800

    leaflet working

--- src/App.js
+++ src/App.js
@@ -1,94 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import NewUserForm from './Components/NewUserForm';
-import EditUserForm from './Components/EditUserForm';
+import React, { useState, useEffect } from "react";
+import NewUserForm from "./Components/NewUserForm";
+import EditUserForm from "./Components/EditUserForm";
+import { MapContainer, TileLayer, useMap ,Marker,Popup} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 const App = () => {
-
   const initialFormState = {
-    id: '',
-    name: '',
-    email: ''
-  }
+    id: "",
+    name: "",
+    email: "",
+  };
 
-  const [users, setUsers] = useState([])
-  const [currentUser, setCurrentUser] = useState(initialFormState)
-  const [editing, setEditing] = useState(false)
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
     fetchUsers();
-  }, [])
+  }, []);
 
   const fetchUsers = async () => {
-    const result = await fetch(`http://localhost:8080/users`)
+    const result = await fetch(`http://localhost:8080/users`);
     result
       .json()
-      .then(result => setUsers(result))
-      .catch(e => console.log(e))
-  }
+      .then((result) => setUsers(result))
+      .catch((e) => console.log(e));
+  };
 
-  const handleInputChange = event => {
-    const { id, value } = event.target
-    setCurrentUser({ ...currentUser, [id]: value })
-  }
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setCurrentUser({ ...currentUser, [id]: value });
+  };
 
   const submitNewUser = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const response = await fetch('http://localhost:8080/users', {
-      method: 'POST',
+    const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(currentUser),
-    })
+    });
     response
       .json()
-      .then(result => setUsers(result))
-      .catch(e => console.log(e))
+      .then((result) => setUsers(result))
+      .catch((e) => console.log(e));
 
-    fetchUsers()
-    setCurrentUser(initialFormState)
-  }
+    fetchUsers();
+    setCurrentUser(initialFormState);
+  };
 
   const deleteUser = async (item) => {
     const response = await fetch(`http://localhost:8080/users/${item.id}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
+    });
     response
       .json()
-      .then(result => setUsers(result), fetchUsers())
-      .catch(e => console.log(e))
-  }
+      .then((result) => setUsers(result), fetchUsers())
+      .catch((e) => console.log(e));
+  };
 
-  const editUser = item => {
-    console.log(item)
-    setEditing(true)
-    setCurrentUser({ id: item.id, name: item.name, email: item.email })
-  }
+  const editUser = (item) => {
+    console.log(item);
+    setEditing(true);
+    setCurrentUser({ id: item.id, name: item.name, email: item.email });
+  };
 
   const submitUserEdit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const response = await fetch(`http://localhost:8080/users/${currentUser.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(currentUser),
-    })
+    const response = await fetch(
+      `http://localhost:8080/users/${currentUser.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(currentUser),
+      }
+    );
     response
       .json()
-      .then(result => setUsers(result))
-      .catch(e => console.log(e))
+      .then((result) => setUsers(result))
+      .catch((e) => console.log(e));
 
-    fetchUsers()
-    setCurrentUser(initialFormState)
-    setEditing(false)
-
-  }
+    fetchUsers();
+    setCurrentUser(initialFormState);
+    setEditing(false);
+  };
 
   return (
     <div className="container">
@@ -96,7 +99,7 @@ const App = () => {
       <h5>A simple app to create, read, update and delete data</h5>
 
       <div className="flex-row">
-        {editing ?
+        {editing ? (
           <div className="flex-large">
             <EditUserForm
               submitUserEdit={submitUserEdit}
@@ -104,7 +107,7 @@ const App = () => {
               currentUser={currentUser}
             />
           </div>
-          :
+        ) : (
           <div className="flex-large">
             <NewUserForm
               submitNewUser={submitNewUser}
@@ -112,7 +115,7 @@ const App = () => {
               currentUser={currentUser}
             />
           </div>
-        }
+        )}
 
         <div className="flex-large">
           <table>
@@ -124,22 +127,44 @@ const App = () => {
               </tr>
             </thead>
             <tbody>
-              {users.map(item =>
+              {users.map((item) => (
                 <tr key={item.id}>
                   <td>{item.name}</td>
                   <td>{item.email}</td>
                   <td>
-                    <button onClick={() => editUser(item)} className="muted-button" >Edit</button>
-                    <button onClick={() => deleteUser(item)} style={{ marginLeft: 5 }} className="muted-button" >Delete</button>
+                    <button
+                      onClick={() => editUser(item)}
+                      className="muted-button"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => deleteUser(item)}
+                      style={{ marginLeft: 5 }}
+                      className="muted-button"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
-              )}
+              ))}
             </tbody>
           </table>
         </div>
       </div>
+      <MapContainer style={{height:'600px'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
-}
+};
 
 export default App;
